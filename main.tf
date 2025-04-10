da2effe424d455d936ece7ae9a2d24820091cd17
StiflerMasuku <sifiso.masuku@dimensiondata.com>
2025-04-10 12:32:36 +0200

    Update main.tf
    Updated script

--- main.tf
+++ main.tf
@@ -271,13 +271,128 @@ resource "aws_connect_user" "AgentIV" {
     phone_type                    = "SOFT_PHONE"
   }
 }
+
+data "aws_connect_prompt" "Emergency_Prompt" {
+  instance_id = "70fb22ed-9bcf-47c7-b442-6c2307be4e2c"
+  name        = "Emergency_Prompt"
+}
+
+data "aws_connect_prompt" "Welcome_Prompt" {
+  instance_id = "70fb22ed-9bcf-47c7-b442-6c2307be4e2c"
+  name        = "Welcome_Prompt"
+}
+data "aws_connect_prompt" "After_Hours_Prompt" {
+  instance_id = "70fb22ed-9bcf-47c7-b442-6c2307be4e2c"
+  name        = "After_Hours"
+}
+
+
+
+
+
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "lambda_function.py"
+  output_path = "DynamoContactCenterClosure.zip"
+}
+
+resource "aws_lambda_function" "DynamoContactCenterClosure" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = "DynamoContactCenterClosure.zip"
+  function_name = "DynamoContactCenterClosure"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "lambda_function.lambda_handler"
+
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  runtime = "Python 3.13"
+}
+
+
+
+data "aws_iam_policy_document" "lambda_logging" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+  
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "dynamodb:*",
+
+    ]
+    resources = [aws_dynamodb_table.DynamoContactCenter.arn]
+  }
+}
+
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+  policy      = data.aws_iam_policy_document.lambda_logging.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
+
+
+resource "aws_dynamodb_table" "DynamoContactCenter" {
+  name           = "DynamoContactCenter"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "PhoneNumber"
+ 
+  attribute {
+    name = "PhoneNumber"
+    type = "S"
+  }
+}
+
+
 resource "aws_connect_contact_flow" "Virgin-Active-Main-Contact-Flow" {
   instance_id  = "70fb22ed-9bcf-47c7-b442-6c2307be4e2c"
   name         = "Virgin-Active-Main-Contact-Flow"
   description  = "Virgin Active Main Contact Flow"
   type         = "CONTACT_FLOW"
   filename     = "Virgin-Active-Main-Line-Flow.json"
-  content_hash = filebase64sha256("Virgin-Active-Main-Line-Flow.json")
+  content = templatefile("./Virgin-Active-Main-Line-Flow.json", {
+    queue_arn = aws_connect_queue.Virgin-Active-Agent-Queue.queue_id.arn
+    hours_arn = aws_connect_hours_of_operation.Virgin-Active-Hours_of_Operation.arn
+    After_Hours_arn = data.aws_connect_prompt.After_Hours_Prompt.arn
+    Welcome_Prompt_arn = data.aws_connect_prompt.Welcome_Prompt.arn
+    Emergency_Prompt_arn = data.aws_connect_prompt.Emergency_Prompt.arn
+    lambda_arn = aws_lambda_function.DynamoContactCenterClosure.arn
+
+  })
   tags = {
     "Name"        = "Virgin Active Main Contact Flow",
     "Application" = "Terraform",
